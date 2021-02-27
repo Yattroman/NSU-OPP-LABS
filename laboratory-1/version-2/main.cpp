373d6e27cdfd10a164241f5dfb07c305d7593d84
machles <machlestv@gmail.com>
2021-02-27 12:25:24 +0700

    Moved on C++ !!!

--- laboratory-1/version-2/main.cpp
+++ laboratory-1/version-2/main.cpp
@@ -1,4 +1,19 @@
-//
-// Created by yattroman on 27.02.2021.
-//
+#include <mpi.h>
+#include <iostream>
+#include <unistd.h>
 
+int main(int argc, char** argv)
+{
+    int  size, rank;
+    char host[32];
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    gethostname(host,32);
+    printf("Hello, MPI world! I'm number %d from %d and I run on host %s.\n", rank, size, host);
+
+    MPI_Finalize();
+    return 0;
+}
