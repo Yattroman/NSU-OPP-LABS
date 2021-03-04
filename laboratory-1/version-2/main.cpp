b73d42783e842e61a04c9da25bcda354c7c502eb
machles <machlestv@gmail.com>
2021-03-04 20:42:42 +0700

    Before serious changes.

--- laboratory-1/version-2/main.cpp
+++ laboratory-1/version-2/main.cpp
@@ -7,9 +7,9 @@
 
 #define EPSILON 0.0001
 
-void distributeData(double* vectorX, double* vectorB, int N){
+void distributeData(double* vectorX, double* vectorB, int* sendcounts, int* recvcounts, int* displs, int procRank, double* vecBPart, int N){
     MPI_Bcast(vectorX, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(vectorB, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Allgatherv(vecBPart, sendcounts[procRank], MPI_DOUBLE, vectorB, recvcounts, displs, MPI_DOUBLE, MPI_COMM_WORLD);
 }
 
 void fillDisplsAndRecvcountsTables(int* displs, int* recvcounts, int* sendcounts, int rowNum, int lastRowAdding, int N, int procSize){
@@ -44,8 +44,8 @@ int main(int argc, char** argv)
     double* vecU = (double*) calloc(N, sizeof(double));
     double* x0 = (double*) malloc(N*sizeof(double));
     double* vecB = (double*) calloc(N, sizeof(double));
-    double* r0; // r0 = b - Ax0, где x0 - нулевой вектор
-    double* z0; // z0 = r0
+    double* r0 = (double*) calloc(N, sizeof(double));       // r0 = b - Ax0, где x0 - нулевой вектор
+    double* z0 = (double*) calloc(N, sizeof(double));       // z0 = r0
 
     double* r[] = {r0, NULL};
     double* z[] = {z0, NULL};
@@ -80,38 +80,31 @@ int main(int argc, char** argv)
         printVector(vecBPart, rowNum);
     }
 
-    std::cout << "Rank: " << procRank << " - " << displs[procRank] << "\n";
+    distributeData(x0, vecB, sendcounts, recvcounts, displs, procRank, vecBPart, N);
 
-    //distributeData(x0, vecB, N);
+    memcpy(r0, vecB, N);              // r0 = b - Ax0, где x0 - нулевой вектор
+    memcpy(z0, r0, N);                // z0 = r0
 
-    //r0 = vecB; // r0 = b - Ax0, где x0 - нулевой вектор
-    //memcpy(z0, r0, N); // z0 = r0
-
-    //printProcRows(mProcRows, rowNum, N);
-    if(procRank == 0){
-        printVector(vecB, N);
-    }
-
-    /*
+    int rowNumMod = (procRank == 0) ? rowNum+lastRowAdding : rowNum;
 
     while (1){
         double* temp[] = {NULL, NULL, NULL, NULL};
 
-        temp[0] = mulMatrixAndVector(rowNum, N, mProcRows, z[0]);                      // Az(k)
-        alpha[1] = scalarVectorAndVector(N, r[0], r[0])
-                   / scalarVectorAndVector(N, temp[0], z[0]);      // alpha(k+1) = (r(k), r(k)) / (Az(k), z(k))
+        temp[0] = mulMatrixAndVector(rowNumMod, N, mProcRows, z[0]);                      // Az(k)
+        alpha[1] = scalarVectorAndVector(rowNumMod, r[0], r[0])
+                   / scalarVectorAndVector(rowNumMod, temp[0], z[0]);      // alpha(k+1) = (r(k), r(k)) / (Az(k), z(k))
 
-        temp[1] = mulVectorAndScalar(N, alpha[1], z[0]);
-        x[1] = sumVectorAndVector(N, x[0], temp[1]);            // x(k+1) = x(k) + alpha(k+1)z(k)
+        temp[1] = mulVectorAndScalar(rowNumMod, alpha[1], z[0]);
+        x[1] = sumVectorAndVector(rowNumMod, x[0], temp[1]);            // x(k+1) = x(k) + alpha(k+1)z(k)
 
-        temp[2] = mulVectorAndScalar(N, alpha[1], temp[0]);
-        r[1] = subVectorAndVector(N, r[0], temp[2]);            // r(k+1) = r(k) - alpha(k+1)Az(k)
+        temp[2] = mulVectorAndScalar(rowNumMod, alpha[1], temp[0]);
+        r[1] = subVectorAndVector(rowNumMod, r[0], temp[2]);            // r(k+1) = r(k) - alpha(k+1)Az(k)
 
-        beta[1] = scalarVectorAndVector(N, r[1], r[1])
-                  / scalarVectorAndVector(N, r[0], r[0]);         // b(k+1) = (r(k+1), r(k+1)) / (r(k), r(k))
+        beta[1] = scalarVectorAndVector(rowNumMod, r[1], r[1])
+                  / scalarVectorAndVector(rowNumMod, r[0], r[0]);         // b(k+1) = (r(k+1), r(k+1)) / (r(k), r(k))
 
-        temp[3] = mulVectorAndScalar(N, beta[1], z[0]);
-        z[1] = sumVectorAndVector(N, r[1], temp[3]);            // z(k+1) = r(k+1) + beta(k+1)z(k)
+        temp[3] = mulVectorAndScalar(rowNumMod, beta[1], z[0]);
+        z[1] = sumVectorAndVector(rowNumMod, r[1], temp[3]);            // z(k+1) = r(k+1) + beta(k+1)z(k)
 
         alpha[0] = alpha[1];
         beta[0] = beta[1];
@@ -125,10 +118,11 @@ int main(int argc, char** argv)
         r[0] = r[1];
 
         for (size_t ui = 0; ui < 4; ++ui) {
-            delete[] temp[ui];
+            free(temp[ui]);
         }
 
         ++repeats;
+
         if( (vectorLength(N, r[1]) / vectorLength(N, vecB) ) < EPSILON){    // |r(k+1)| / |b| < EPSILON
             break;
         }
@@ -136,7 +130,6 @@ int main(int argc, char** argv)
 
 //    printVector(vecU, N);
 //    printVector(x[1], N);
-     */
 
     MPI_Finalize();
 
