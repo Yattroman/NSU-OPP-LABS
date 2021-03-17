731b7d208b3ad9ab088aa5c75a58120ad2ee3062
machles <machlestv@gmail.com>
2021-03-17 20:58:25 +0700

    3rd lab init

---
+++ laboratory-2/version-3/mv/mvOperations_l2_3rd.cpp
@@ -0,0 +1,67 @@
+#include <cstring>
+#include "mvOperations_l2_3rd.h"
+
+/*
+void printMatrix(double* matrix, int N){
+    for(size_t i = 0; i < N; ++i){
+        for(size_t j = 0; j < N; ++j){
+            printf("%f ", matrix[j + i*N]);
+        }
+        printf("\n");
+    }
+}
+*/
+
+void printVector(double* vector, int N){
+    for(size_t i = 0; i < N; ++i){
+        printf("%f ", vector[i]);
+    }
+    printf("\n");
+}
+
+void subVectorAndVector(int N, const double* vectorL, const double* vectorR, double * res){
+    #pragma omp parallel for
+    for(size_t j = 0; j < N; ++j){
+        res[j] = vectorL[j] - vectorR[j];
+    }
+}
+
+void sumVectorAndVector(int N, const double* vectorL, const double* vectorR, double * res){
+    #pragma omp parallel for
+    for(size_t j = 0; j < N; ++j){
+        res[j] = vectorL[j] + vectorR[j];
+    }
+}
+
+void mulMatrixAndVector(int N, const double* matrix, const double* vector, double * res){
+    #pragma omp parallel for
+    for(size_t i = 0; i < N; ++i) {
+        res[i] = 0;
+        #pragma omp parallel for
+        for (size_t j = 0; j < N; ++j) {
+            res[i] += matrix[i*N + j] * vector[j];
+        }
+    }
+}
+
+double scalarVectorAndVector(int N, const double* vectorL, const double* vectorR){ // Memory OK
+    double res = 0;
+
+    #pragma omp parallel for reduction (+: res)
+    for(size_t i = 0; i < N; ++i) {
+        res += vectorL[i] * vectorR[i];
+    }
+
+    return res;
+}
+
+double vectorLength(int N, const double* vector){ // Memory OK
+    return sqrt(scalarVectorAndVector(N, vector, vector));
+}
+
+void mulVectorAndScalar(int N, double scalar, const double* vector, double * res){
+    #pragma omp parallel for
+    for(size_t i = 0; i < N; ++i) {
+        res[i] = scalar * vector[i];
+    }
+}
